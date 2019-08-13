3c0f74dba4fd2340d341d2c190013eac3749ae7f
JCRN <clemons.jason@gmail.com>
2019-08-13 14:55:30 -0700

    initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import dependencies
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// import stylesheets
+import './temp.scss'
+import './stylesheets/main.scss'
+
+// import components
+import App from './App'
+
+ReactDOM.render(<App />, document.getElementById('root'))
